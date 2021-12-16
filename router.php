aecebc480620040f6d61180b28a8ec750b95d8ef
Nicolás García <nicolas.garcia.r@ug.uchile.cl>
2021-12-16 10:27:19 -0300

    Se crea servidor y router de API en php

---
+++ router.php
@@ -0,0 +1,30 @@
+<?php
+
+$matches = [];
+
+// Verifica expresion regular de la forma "/(A-Z)*/(A-Z)*"
+// En nuestro caso, tenemos "/<recurso>/<id_recurso>"
+if (preg_match('/\/([^\/]+)\/([^\/]+)/', $_SERVER["REQUEST_URI"], $matches)) {
+    // Conseguimos los valores internos del url y les hacemos match con nuestro servidor
+    $_GET['resource_type'] = $matches[1];
+    $_GET['resource_id'] = $matches[2];
+
+    error_log( print_r($matches, 1) );
+    // Llamamos al servidos.php
+    require 'server.php';
+
+// Verifica expresion regular del tipo "/(A-Z)*"
+// En nuestro caso, tenemos "/<recurso>"
+} elseif ( preg_match('/\/([^\/]+)\/?/', $_SERVER["REQUEST_URI"], $matches) ) {
+    $_GET['resource_type'] = $matches[1];
+    error_log( print_r($matches, 1) );
+
+    // Llamamos al servidos.php
+    require 'server.php';
+
+// Si este no calza con ninguna expresion, lanzamos error
+} else {
+
+    error_log('No matches');
+    http_response_code( 404 );
+}
---
+++ server.php
@@ -0,0 +1,128 @@
+<?php
+
+header('Content-Type: application/json');
+
+// Definimos los recursos disponibles
+$allowedResourceTypes = [
+	'books',
+	'authors',
+	'genres',
+];
+
+// Validamos que el recurso este disponible
+$resourceType = $_GET['resource_type'];
+if ( !in_array( $resourceType, $allowedResourceTypes ) ) {
+	http_response_code( 400 );
+	echo json_encode(
+		[
+			'error' => "$resourceType is un unkown",
+		]
+	);
+	
+	die;
+}
+
+// NUESTROS DATOS A EXPONER EN CONEXION REST
+$books = [
+	1 => [
+		'titulo' => 'Lo que el viento se llevo',
+		'id_autor' => 2,
+		'id_genero' => 2,
+	],
+	2 => [
+		'titulo' => 'La Iliada',
+		'id_autor' => 1,
+		'id_genero' => 1,
+	],
+	3 => [
+		'titulo' => 'La Odisea',
+		'id_autor' => 1,
+		'id_genero' => 1,
+	],
+];
+
+// Rescatamos el ID ingresado en el GET
+$resourceId = array_key_exists('resource_id', $_GET ) ? $_GET['resource_id'] : '';
+$method = $_SERVER['REQUEST_METHOD'];
+
+// SWITCH DE CASOS GET, PUT, POST, DELETE
+switch ( strtoupper( $method ) ) {
+	// Caso Get
+	case 'GET':
+		// Si no solicitan los libros, entonces arrojamos error
+		if ( "books" !== $resourceType ) {
+			http_response_code( 404 );
+
+			echo json_encode(
+				[
+					'error' => $resourceType.' not yet implemented :(',
+				]
+			);
+
+			die;
+		}
+
+		// Si el ID no est vacío, lo buscamos y lo retornamos
+		if (!empty( $resourceId )) {
+			// Si el id existe, encodeamos el libro con el código
+			if ( array_key_exists( $resourceId, $books ) ) {
+				echo json_encode( $books[ $resourceId ] );
+			// Si no existe, lanzamos error
+			} else {
+				http_response_code( 404 );
+
+				echo json_encode(
+					[
+						'error' => 'Book '.$resourceId.' not found :(',
+					]
+				);
+			}
+
+		// Si todo está bien, entonces retornamos los libros
+		} else {
+			echo json_encode(
+				$books
+			);
+		}
+
+		die;
+		
+		break;
+
+	// Caso POST
+	case 'POST':
+		$json = file_get_contents( 'php://input' );
+		$books[] = json_decode( $json );
+		echo array_keys($books)[count($books)-1];
+		break;
+
+	// Caso PUT
+	case 'PUT':
+		if ( !empty($resourceId) && array_key_exists( $resourceId, $books ) ) {
+			$json = file_get_contents( 'php://input' );
+			
+			$books[ $resourceId ] = json_decode( $json, true );
+
+			echo $resourceId;
+		}
+		break;
+	
+	// Caso DELETE
+	case 'DELETE':
+		if ( !empty($resourceId) && array_key_exists( $resourceId, $books ) ) {
+			unset( $books[ $resourceId ] );
+		}
+		break;
+	default:
+		http_response_code( 404 );
+
+		echo json_encode(
+			[
+				'error' => $method.' not yet implemented :(',
+			]
+		);
+
+		break;
+}
+
+?>
